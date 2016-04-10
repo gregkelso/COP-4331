2871c224c54851653a4bc9d6726ccbd73c4ecc6e
Greg <gregorykelso@gmail.com>
2016-04-10 14:47:34 -0400

    Updated UIController script and implemented Pause menu throughout scenes

--- COP4331Final/Final Game1/Assets/Resources/Scripts/Controllers/UIController.cs
+++ COP4331Final/Final Game1/Assets/Resources/Scripts/Controllers/UIController.cs
@@ -6,6 +6,32 @@ using UnityEngine.SceneManagement;
 
 public class UIController : MonoBehaviour {
 
+	public GameObject PauseUI;
+
+	private bool paused = false;
+
+	void Start() {
+		PauseUI.SetActive (false);
+	}
+
+	void Update() {
+		if (Input.GetButtonDown("Pause")) {
+			paused = !paused;
+		}
+		if (paused) {
+			PauseUI.SetActive (true);
+			Time.timeScale = 0;
+		}
+		if (!paused) {
+			PauseUI.SetActive (false);
+			Time.timeScale = 1;
+		}
+	}
+
+	public void ResumeGame() {
+		paused = false;
+	}
+
 	// Modify the button object's OnClick() properties in the inspector window to set button's destinations
 	public void ChangeScene(string sceneName) {
 		Application.LoadLevel (sceneName);
@@ -14,4 +40,6 @@ public class UIController : MonoBehaviour {
 	public void QuitGame() {
 		Application.Quit ();
 	}
+
+
 }
